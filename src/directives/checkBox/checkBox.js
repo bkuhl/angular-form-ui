303fb3620a3cdd5a647ca3a6d5e527ce2d532dea
BKuhl <benkuhl@gmail.com>
2013-10-31 12:17:42 -0400

    Initial checkbox implementation

--- src/directives/checkBox/checkBox.js
+++ src/directives/checkBox/checkBox.js
@@ -2,58 +2,52 @@ angular.module('angular-form-ui').
     /**
      * <check-box ng-model="isChecked()"></check-box>
      * Required attribute: ng-model="[expression]"
+     * Optional attribute: value="[expression]"
      */
     directive('checkBox', function () {
         return {
             replace: true,
             restrict: 'E',
-            //require: '^ngModel',
             scope: {
                 'externalValue': '=ngModel',
                 'value': '&'
             },
-            template: function (el, attrs) {
-                var html =
-                    '<div class="ngCheckBox">'+
-                        '<span ng-class="{checked: isChecked}">' +
-                            '<input type="checkbox" ng-model="isChecked"/>'+
-                        '</span>'+
-                    '</div>';
-                return html;
-            },
-            controller: ['$scope', '$timeout', function ($scope, $timeout) {
-                var initialized = false;
-
+            template:
+                '<div class="ngCheckBox">'+
+                    '<span ng-class="{checked: isChecked}">' +
+                        '<input type="checkbox" ng-model="isChecked"/>'+
+                    '</span>'+
+                '</div>'
+            ,
+            controller: ['$scope', function ($scope) {
                 if (angular.isArray($scope.externalValue)) {
-                    $scope.isChecked = $scope.externalValue.indexOf($scope.value()) > 0;
+                    $scope.isChecked = $scope.externalValue.indexOf($scope.value()) >= 0;
                 } else {
                     $scope.isChecked = !!$scope.externalValue;
                 }
 
-                $scope.$watch('isChecked', function (newValue) {
-                    if (angular.isDefined(newValue)) {
+                $scope.$watch('isChecked', function (newValue, oldValue) {
+                    if (angular.isDefined(newValue) && angular.isDefined(oldValue)) {
                         //add or remove items if this is an array
                         if (angular.isArray($scope.externalValue)) {
                             var index = $scope.externalValue.indexOf($scope.value());
-                            if(index > -1) {
-                                //delete $scope.externalValue[$scope.value()];
-                                $scope.externalValue.splice(index, 1);
-                            } else if (initialized) {
-                                //$scope.externalValue[$scope.value()] = null;
-                                $scope.externalValue.push($scope.value());
+                            if(newValue) {
+                                if( index < 0 ) $scope.externalValue.push($scope.value());
+                            } else {
+                                if( index >= 0 ) $scope.externalValue.splice(index, 1);
                             }
                         } else {
                             //simple boolean value
                             $scope.externalValue = newValue;
                         }
-                        if (initialized)
-                            console.log($scope.externalValue);
                     }
-                });
-
-                $timeout(function () {
-                    initialized = true;
                 });
             }]
         };
     });
+
+tApp.directive("enter", function () {
+    return {
+        scope: false
+    };
+})
